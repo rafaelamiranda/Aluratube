0a0b538c2f5cc8d8c918a1d16d1d9a97e2822a3c
Rafaela Miranda <rafaela-gomes@outlook.com>
2022-11-09 19:02:23 -0300

    Aula 03

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
@@ -10,16 +9,16 @@ function HomePage() {
 
 	return (
 		<>
-			<CSSReset />
 			<div style={{
 				display: "flex",
 				flexDirection: "column",
 				flex: 1,
+				// backgroundColor: "red",
 			}}>
 				{/* Prop Drilling */}
 				<Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
 				<Header />
-				<Timeline searchValue={valorDoFiltro} playlists={config.playlists} >
+				<Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
 					Conteúdo
 				</Timeline>
 				<Aluratubes favorites={config.favorites} />
@@ -30,72 +29,91 @@ function HomePage() {
 
 export default HomePage
 
-const StyledHeader = styled.div`
-	.avatar {
-		width: 80px;
-		height: 80px;
-		border-radius: 50%;
-	}
-	.user-info {
-		display: flex;
-		align-items: center;
-		width: 100%;
-		padding: 16px 32px;
-		gap: 16px;
-	}
-`;
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
 
+
+const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
 const StyledBanner = styled.div`
 	background-image: url(${({ bg }) => bg});
 	background-size: cover;
 	height: 230px;
 `;
-
 function Header() {
 	return (
 		<StyledHeader>
 			<StyledBanner bg={config.banner} />
 			<section className="user-info">
-				<img src={`https://github.com/${config.github}.png`} className="avatar" />
+				<img src={`https://github.com/${config.github}.png`} />
 				<div>
-					<h2>{config.name}</h2>
-					<p>{config.job}</p>
+					<h2>
+						{config.name}
+					</h2>
+					<p>
+						{config.job}
+					</p>
 				</div>
 			</section>
 		</StyledHeader>
-	);
+	)
 }
 
-function Timeline({searchValue,...props}) {
-	const playlistNames = Object.keys(props.playlists);
+function Timeline({ searchValue, ...propriedades }) {
+	// console.log("Dentro do componente", propriedades.playlists);
+	const playlistNames = Object.keys(propriedades.playlists);
 	// Statement
 	// Retorno por expressão
 	return (
 		<StyledTimeline>
 			{playlistNames.map((playlistName) => {
-				const videos = props.playlists[playlistName];
-				return [
+				const videos = propriedades.playlists[playlistName];
+				// console.log(playlistName);
+				// console.log(videos);
+				return (
 					<section key={playlistName}>
 						<h2>{playlistName}</h2>
 						<div>
-							{videos.filter((video) => {
-								const titleNormalized = video.title.toLowerCase();
-								const searchValueNormalized = searchValue.toLowerCase();
-								return titleNormalized.includes(searchValueNormalized)
-							}).map((video) => {
-								return (
-									<a key={video.url} href={video.url}>
-										<img src={video.thumb} />
-										<span>{video.title}</span>
-									</a>
-								)
-							})}
+							{videos
+								.filter((video) => {
+									const titleNormalized = video.title.toLowerCase();
+									const searchValueNormalized = searchValue.toLowerCase();
+									return titleNormalized.includes(searchValueNormalized)
+								})
+								.map((video) => {
+									return (
+										<a key={video.url} href={video.url}>
+											<img src={video.thumb} />
+											<span>
+												{video.title}
+											</span>
+										</a>
+									)
+								})}
 						</div>
 					</section>
-				]
+				)
 			})}
 		</StyledTimeline>
-	);
+	)
 }
 
 const StyledAluratubes = styled.div`
@@ -106,21 +124,21 @@ const StyledAluratubes = styled.div`
 		margin-bottom: 8px;
 		font-size: 14px;
 	}
-
 	a {
 		display: inline-grid;
 		text-align: center;
 		color: #000;
 		margin: 0 8px;
 	}
-
 	section {
 		padding: 0 32px;
 	}
-
 	h2 {
 		font-size: 16px;
 		margin-bottom: 16px;
+	}
+	span {
+		color: ${({ theme }) => theme.textColorBase};
 	}
 `;
 
