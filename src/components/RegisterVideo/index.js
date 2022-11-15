aa725b8faa8b943f3bb8cefe3399674ee25f309f
Rafaela Miranda <rafaela-gomes@outlook.com>
2022-11-15 19:34:42 -0300

    Aula 05

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -33,7 +33,7 @@ const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 export default function RegisterVideo() {
 	const formCadastro = useForm({
-		initialValues: { titulo: "Frost punk", url: "https://www.youtube.com/watch?v=QsqatJxAUtk" }
+		initialValues: { titulo: "", url: "" }
 	});
 	const [formVisivel, setFormVisivel] = React.useState(false);
 
